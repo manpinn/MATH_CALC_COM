ec61663708eba81b3959cc65f484b2d874f165d5
Mario Kozlovac <mario.kozlovac@outlook.com>
2024-07-27 22:26:49 +0200

    test models

---
+++ MATH_CALC_COM/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using MATH_CALC_COM.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MATH_CALC_COM.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            Test testObject = new Test() { Teststring = "MKO Schon" };
+            return View(testObject);
+        }
+
+        public IActionResult MKO1()
+        {
+            Test testObject = new Test() { Teststring = "MKO Schon" };
+            return View("MKO1", testObject);
+        }
+    }
+}
---
+++ MATH_CALC_COM/Models/Test.cs
@@ -0,0 +1,7 @@
+﻿namespace MATH_CALC_COM.Models
+{
+    public class Test
+    {
+        public string Teststring { get; set; }
+    }
+}
