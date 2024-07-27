39408dcf9cf2a15bd1ee93638f0fbae3a9e55eaf
Mario Kozlovac <mario.kozlovac@outlook.com>
2024-07-27 22:00:38 +0200

    Projektdateien hinzufügen.

---
+++ MATH_CALC_COM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MATH_CALC_COM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
