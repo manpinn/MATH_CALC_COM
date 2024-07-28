5e8994b0be9c4182eb84326e826f18cd2f51476c
Mario Kozlovac <mario.kozlovac@outlook.com>
2024-07-28 13:11:03 +0200

    navbar buttons

---
+++ MATH_CALC_COM/Controllers/ModuleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MATH_CALC_COM.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
