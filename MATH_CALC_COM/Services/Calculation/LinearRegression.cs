9a16bf475cd36d9c38df09ebe053f9688ad13168
Mario Kozlovac <mario.kozlovac@outlook.com>
2024-07-28 17:30:20 +0200

    chart

--- MATH_CALC_COM/Services/Calculation/LinearRegression.cs
+++ MATH_CALC_COM/Services/Calculation/LinearRegression.cs
@@ -9,18 +9,18 @@ namespace MATH_CALC_COM.Services.Calculation
     {
         public string LinearRegressionTest() 
         {
-            // Assuming you have two sets of y values and corresponding x values
+            
             List<int> xValues = Enumerable.Range(1, 500).ToList();
-            List<float> y1Values = getY1Values(); // Replace with your function or data
+            List<float> y1Values = getY1Values(); 
 
-            // Create a chart for each line plot
-            var chartY1 = Chart2D.Chart.Line<int, float, string>(xValues, y1Values);
+            
+            var chartY1 = Chart2D.Chart.Line<int, float, string>(xValues, y1Values, true, "MKO");
 
-            // Add them to a list
+            
             var chartList = new List<GenericChart>();
             chartList.Add(chartY1);
 
-            // Combine the charts
+            
             var combinedChart = Chart.Combine(chartList);
 
             string json = Newtonsoft.Json.JsonConvert.SerializeObject(combinedChart, new Newtonsoft.Json.JsonSerializerSettings
